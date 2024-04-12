9f6d78f6689718e02acdbe793f14365258592073
Seo <53137698+seo888@users.noreply.github.com>
2024-04-12 06:01:41 +0000

    update

--- lock/__init__.py
+++ lock/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.2 (basic), 004374, 2024-04-10T16:32:41.773925
+# Pyarmor 8.5.2 (basic), 004374, 2024-04-11T13:00:05.983928
 from .pyarmor_runtime import __pyarmor__
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 # -*- coding: UTF-8 -*-
 """
-MyCloak V1.1 | by TG@seo898
+MyCloak V1.2 | by TG@seo898
 """
 
+import asyncio
 import logging
-from logging.handlers import TimedRotatingFileHandler
 from concurrent.futures import ThreadPoolExecutor
 import os
 from fastapi.responses import FileResponse, RedirectResponse
 from func.function import Func
 import uvicorn
+from cacheout import Cache
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import JSONResponse
 from starlette.middleware.gzip import GZipMiddleware
 from starlette.templating import Jinja2Templates
 from func.middleware import Mid
-from func.tg import Telegram
 
 app = FastAPI(access_log=False)
 # gzip流文件处理
@@ -28,6 +28,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.state.func = Func()
 # 初始化ad_count
 app.state.ad_count = 0
+# 初始化缓存器
+app.state.cache = Cache(ttl=300)
 # 线程管理
 app.state.executor = ThreadPoolExecutor(32)
 # 创建一个templates（模板）对象，以后可以重用。
@@ -98,6 +100,33 @@ async def exe(request: Request, response: Response, path=""):
     return RedirectResponse(url=new_path, status_code=301)
 
 
+@app.get("/goto/{path:path}")
+async def goto(request: Request, response: Response, path=""):
+    config = request.state.config
+    res_ip = request.state.res_ip
+    cache = app.state.cache
+    if path in config['【斗篷设置】']['goto模式']['广告链接设置']:
+        ok_link, ad_link = config['【斗篷设置】']['goto模式']['广告链接设置'][path]
+        if res_ip in cache:
+            # 触发跳转
+            tg_mes = f"""【{app.state.ad_count}】{cache[res_ip]} goto:{ad_link}"""
+            app.state.executor.submit(asyncio.run, request.state.tg.send_mes(tg_mes))
+            if ad_link.startswith('http://') or ad_link.startswith('https://'):
+                return RedirectResponse(url=ad_link, status_code=301)
+            datas = {"request": request}
+            return app.state.templates.TemplateResponse(ad_link,
+                                                        datas,
+                                                        media_type="text/html")
+        if ok_link.startswith('http://') or ok_link.startswith('https://'):
+            return RedirectResponse(url=ok_link, status_code=301)
+        datas = {"request": request}
+        return app.state.templates.TemplateResponse(ok_link,
+                                                    datas,
+                                                    media_type="text/html")
+    else:
+        return RedirectResponse(url='/', status_code=301)
+
+
 @app.get("{path:path}")
 async def route(request: Request, response: Response, path=""):
     """主路由"""
@@ -108,10 +137,11 @@ async def route(request: Request, response: Response, path=""):
                                                     datas,
                                                     media_type="text/html")
     mode = request.state.config["【斗篷设置】"]['模式']
-    if mode == '正常':
-        return app.state.templates.TemplateResponse(request.state.config["【蜘蛛策略】"]["正常页面"],
-                                                    datas,
-                                                    media_type="text/html")
+    if mode == '正常' or mode == 'goto':
+        return app.state.templates.TemplateResponse(
+            request.state.config["【蜘蛛策略】"]["正常页面"],
+            datas,
+            media_type="text/html")
     elif mode == '广告':
         ad_path = request.state.config["【斗篷设置】"]["广告地址"]
         if ad_path.startswith('http://') or ad_path.startswith('https://'):
